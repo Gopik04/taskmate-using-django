159f787e2115593f7edfde721d9c0de399982dcb
Gopik04 <gopivkp04@gmail.com>
2024-12-12 22:29:06 +0530

    form added and views updated

--- todolist_app/views.py
+++ todolist_app/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import TaskList
+from .forms import TaskForm
+from django.contrib import messages
 # Create your views here.
 def todolist(request):
-   all_task=TaskList.objects.all
-   return render(request,'todolist.html',{'all_task':all_task})
+   if request.method == 'POST':
+      form=TaskForm(request.POST)
+      if form.is_valid():
+         form.save()
+      messages.success(request,'Task Added Successfully !!!')
+      return redirect('todolist')
+   else:
+      all_task=TaskList.objects.all
+      return render(request,'todolist.html',{'all_task':all_task})
 
 def contact(request):
    context={'contact_text':'welcome to contact page'}
