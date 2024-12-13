7da123547ce660e1b8a9b5cd90099fb451629cd9
Gopik04 <gopivkp04@gmail.com>
2024-12-13 09:52:23 +0530

    edit and delete fields are added

--- todolist_app/urls.py
+++ todolist_app/urls.py
@@ -3,6 +3,8 @@ from.import views
 
 urlpatterns = [
   path('',views.todolist,name='todolist'),
+  path('delete/<task_id>',views.delete_task,name='delete_task'),
+   path('edit_task/<task_id>',views.edit_task,name='edit_task'),
   path('contactus/',views.contact,name='contactus'),
   path('aboutus/',views.about,name='aboutus')
 ]
--- todolist_app/views.py
+++ todolist_app/views.py
@@ -15,6 +15,23 @@ def todolist(request):
       all_task=TaskList.objects.all
       return render(request,'todolist.html',{'all_task':all_task})
 
+def delete_task(requst,task_id):
+   task=TaskList.objects.get(pk=task_id)
+   task.delete()
+   return redirect('todolist')
+
+def edit_task(request,task_id):
+   if request.method == 'POST':
+      task=TaskList.objects.get(pk=task_id)
+      form=TaskForm(request.POST or None, instance = task)
+      if form.is_valid():
+         form.save()
+      messages.success(request,'Task Edited Successfully !!!')
+      return redirect('todolist')
+   else:
+      task_obj=TaskList.objects.get(pk=task_id)
+      return render(request,'edit.html',{'task_obj':task_obj})
+   
 def contact(request):
    context={'contact_text':'welcome to contact page'}
    return render(request,'contact.html',context)
