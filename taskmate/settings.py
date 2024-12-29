8d5215b9ff4e4ff09d5ddb61e448c37dc90ce343
Gopik04 <gopivkp04@gmail.com>
2024-12-29 16:13:06 +0530

    setting file updated

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -86,7 +86,11 @@ WSGI_APPLICATION = 'taskmate.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': env('DJANGO_DB_NAME'),
+        'USER': env('DJANGO_DB_USER'),
+        'PASSWORD': env('DJANGO_DB_PASSWORD'),
+        'HOST': env('DJANGO_DB_HOST'),
+        'PORT': env('DJANGO_DB_PORT'),
     }
 }
 
